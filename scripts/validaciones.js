ff6eec6906855fa0ade5e94b311a184c9a695985
Ema Gomez <emagozz@gmail.com>
2023-04-12 21:27:43 -0300

    WIP

--- scripts/validaciones.js
+++ scripts/validaciones.js
@@ -20,8 +20,10 @@ const validacionImg = () => {
     const getInputValue = document.getElementById('form-principal').querySelector('#img-juego').value;
 
     const getFinalExtension = getInputValue.substring(getInputValue.indexOf('.') + 1).trim(); // trim() elimina los espacios que rodean el texto.
+    const getFileName = getInputValue.substring(getInputValue.lastIndexOf('\\') + 1, getInputValue.lastIndexOf('.'));
+
     if(getFinalExtension === 'jpg' || getFinalExtension === 'png') {
-        document.getElementById('error-img').innerHTML = ''
+        document.getElementById('info-img').innerHTML = 'Nombre del archivo: ' + getFileName
     } else {
         document.getElementById('error-img').innerHTML = 'Extension no soportada'
     }
@@ -30,18 +32,18 @@ const validacionImg = () => {
 const validacionDescripcion = () => {
     const getInputValue = document.getElementById('form-principal').querySelector('#descripcion').value;
 
-    document.getElementById('error-descrip').innerHTML=getInputValue.length>255?'No debe superar 255 caracteres.':'';
+    document.getElementById('error-descrip').innerHTML= getInputValue.length > 255 ? 'No debe superar 255 caracteres.' : '';
 }
 
 const validacionPlataforma = () => {
     const getInputValue = document.getElementById('form-principal').querySelector('#plataforma').value;
 
-    document.getElementById('error-plataforma').innerHTML=getInputValue>0?'':'Campo requerido.';
+    document.getElementById('error-plataforma').innerHTML= getInputValue> 0 ? '' : 'Campo requerido.';
 }
 
 const validacionUrl = () => {
     const getInputValue = document.getElementById('form-principal').querySelector('#url').value;
 
-    document.getElementById('error-url').innerHTML=getInputValue.length>80?'No debe superar 80 caracteres.':'';
+    document.getElementById('error-url').innerHTML= getInputValue.length > 80 ? 'No debe superar 80 caracteres.' : '';
 }
 
