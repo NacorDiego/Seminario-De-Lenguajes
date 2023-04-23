6d637f8f443438354e838b803d6144696e02573a
Ema Gomez <emagozz@gmail.com>
2023-04-23 18:25:52 -0300

    Add comments

--- scripts/filtros.js
+++ scripts/filtros.js
@@ -1,6 +1,10 @@
 const filtros = (event) =>{
+  //Evitamos que cada vez que se mande un submit se vuelva a cargar la pagina
   event.preventDefault();
+  //Mostramos todos los juegos
   Array.from(document.querySelectorAll('.titulo-juego')).forEach(item => item.parentElement.parentElement.parentElement.style.display = '')
+
+  //Obtenemos los valores de los filtros
   const nombre = document.querySelector('#nombre').value
   const plataforma = document.querySelector('#plataforma').value
   const genero = document.querySelector('#genero').value
