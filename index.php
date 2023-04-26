e98178d2fb5ff70207cd030f00a4f0cf2e313343
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-04-26 10:53:47 -0300

    Se agrega fuente Montserrat. Cambio de fuente en titulos de juegos. Aumento el tamaño de la fuente de los encabezados.

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- FUENTES -->
+    <!-- FUENTE - ROBOTO -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- FUENTE - MONTSERRAT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <!-- MIS ESTILOS -->
     <link rel="stylesheet" href="estilos.css">
     <link rel="icon" href="./imgs/logo.svg"/>
