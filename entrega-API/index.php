8516826150193177c9397c085096b9745e90de1c
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-05-22 17:10:16 -0300

    Agrego 'obtenerGeneros' con validación de errores 400 y 404. Agrego comentarios y cambios menores.

--- entrega-API/index.php
+++ entrega-API/index.php
@@ -13,38 +13,40 @@
     $db = new Db();
 
     // Llamar al método connect()
-    $db->connect();
+    $db -> connect();
 
     //Todas las funciones se repiten pero cambian pocas cosas, falta agregar errores 404 y 400. Por ahora esta todo sin catchear errores
     //Investigar como mover todos los endpoints de genero a otro archivo y modularizarlo.
-    $app->post('/agregarGeneros', function (Request $request, Response $response, $args) use ($db) {
+
+    //? Chequear si los args se pueden borrar
+    $app -> post('/agregarGeneros', function (Request $request, Response $response, $args) use ($db) {
         //getBody obtiene todas las solicitudes enviadas al servidor (POST) //Modularizar
-        $requestBody = $request->getBody();
+        $requestBody = $request -> getBody();
         //json_decode convierte el json en un array asociativo (basicamente que PHP pueda interpretar la data JSON)
         $data = json_decode($requestBody, true);
         //en $data estan todos las props que mandamos desde Postman
         $nombreGenero = $data['nombreGenero'];
 
         // Obtén la conexión a la base de datos
-        $connection = $db->getConnection();
+        $connection = $db -> getConnection();
 
         // Insertar el nuevo género en la base de datos
-        $sqlInsert = $connection->prepare("INSERT INTO `generos`(`nombre`) VALUES (?)");
+        $sqlInsert = $connection -> prepare("INSERT INTO `generos`(`nombre`) VALUES (?)");
         // Agregar try/catch si falla el execute con 404 creo
         //Donde va el signo de pregunta es donde se inserta la variable
-        $sqlInsert->execute([$nombreGenero]);
+        $sqlInsert -> execute([$nombreGenero]);
 
         // Devolver una respuesta exitosa // Modularizar
         $jsonData = json_encode(['message' => 'Genero creado exitosamente']);
-        $responseBody = $response->getBody();
-        $responseBody->write($jsonData);
-        return $response->withStatus(200)->withBody($responseBody);
+        $responseBody = $response -> getBody();
+        $responseBody -> write($jsonData);
+        return $response -> withStatus(200) -> withBody($responseBody);
     });
 
-
+    //? Chequear si los args se pueden borrar
     $app->post('/actualizarGenero', function (Request $request, Response $response, $args) use ($db){
         // Obtiene la petición en formato json
-        $requestBody = $request->getBody();
+        $requestBody = $request -> getBody();
         // Transforma el json a un array y lo guarda en $data
         $data = json_decode($requestBody, true);
         // Obtiene los datos que vienen en "nombreGenero" e "idGenero" y los guarda en las variables
@@ -52,37 +54,89 @@
         $idGenero = $data['idGenero'];
 
         // Obtiene la conexión a la bd mediante la instancia de Db.
-        $connection = $db->getConnection();
+        $connection = $db -> getConnection();
 
         // Guarda en $sqlUpdate la query preparada para actualizar el genero de nombre=? y id=?
-        $sqlUpdate = $connection->prepare("UPDATE `generos` SET `nombre` = ? WHERE `id` = ?");
+        $sqlUpdate = $connection -> prepare("UPDATE `generos` SET `nombre` = ? WHERE `id` = ?");
         // Ejecuta la query con los datos pasados como param en la BD.
-        $sqlUpdate->execute([$nombreGenero, $idGenero]);
+        $sqlUpdate -> execute([$nombreGenero, $idGenero]);
 
         // Guarda el msj en un array y lo encodea a json
         $jsonData = json_encode(['message' => 'Genero actualizado exitosamente']);
         // Almacena la referencia al cuerpo de la respuesta en $responseBody
-        $responseBody = $response->getBody();
+        $responseBody = $response -> getBody();
         // Escribe en el cuerpo de la respuesta el json guardado con el msj anteriormente
-        $responseBody->write($jsonData);
+        $responseBody -> write($jsonData);
         // Retorna una respuesta HTTP con codigo 200 (éxito) y el cuerpo con el json
-        return $response->withStatus(200)->withBody($responseBody);
+        return $response -> withStatus(200) -> withBody($responseBody);
     });
 
+    //? Chequear si los args se pueden borrar
     $app->post('/eliminarGenero', function (Request $request, Response $response, $args) use ($db){
-        $requestBody = $request->getBody();
+        $requestBody = $request -> getBody();
         $data = json_decode($requestBody, true);
         $idGenero = $data['idGenero'];
 
-        $connection = $db->getConnection();
+        $connection = $db -> getConnection();
 
-        $sqlUpdate = $connection->prepare("DELETE FROM `generos` WHERE `id` = ?");
-        $sqlUpdate->execute([$idGenero]);
+        $sqlDelete = $connection -> prepare("DELETE FROM `generos` WHERE `id` = ?");
+        $sqlDelete -> execute([$idGenero]);
 
         $jsonData = json_encode(['message' => 'Genero elimnado exitosamente']);
-        $responseBody = $response->getBody();
-        $responseBody->write($jsonData);
-        return $response->withStatus(200)->withBody($responseBody);
+        $responseBody = $response -> getBody();
+        $responseBody -> write($jsonData);
+        return $response -> withStatus(200) -> withBody($responseBody);
+    });
+
+    //? Chequear si request debe ir o se puede borrar en los param
+    $app -> get('/obtenerGeneros', function (Request $request, Response $response) use ($db){
+        // Conecto a la BD.
+        $connection = $db -> getConnection();
+        // Defino query
+        $sqlSelect = $connection -> prepare("SELECT DISTINCT * FROM `generos`");
+
+        if (!$sqlSelect){
+            // Si hay un error en la preparación de la consulta:
+
+            $errorData = ['error' => 'Error en la consulta'];
+            $errorJson = json_encode($errorData);
+            $responseBody = $response -> getBody();
+            $responseBody -> write($errorJson);
+            return $response -> withStatus(400) -> withBody($responseBody);
+        }
+
+        if ($sqlSelect -> execute()){
+            // Si se ejecuta la consulta correctamente en la BD:
+
+            // Ejecuto la query en la BD.
+            $sqlSelect -> execute();
+            // Obtengo los datos devueltos por la BD y los guardo en $result
+            $result = $sqlSelect -> fetchAll(PDO::FETCH_ASSOC);
+            // Encodeo los datos como json
+            $jsonData = json_encode($result);
+            // Obtengo una instancia del cuerpo de la respuesta
+            $responseBody = $response -> getBody();
+            // Guardo la data en el cuerpo de la respuesta
+            $responseBody -> write($jsonData);
+            // Retorno la respuesta
+            return $response -> withStatus(200) -> withBody($responseBody);
+        } else {
+            // Si hay un error en la ejecución de la consulta:
+
+            $errorData = ['error' => 'Error en la ejecución de la consulta a la BD.'];
+            $errorJson = json_encode($errorData);
+            $responseBody = $response -> getBody();
+            $responseBody -> write($errorJson);
+            return $response -> withStatus(400) -> withBody($responseBody);
+        }
+
+        // Si la tabla indicada no existe (no se ejecuta ninguno de los return anteriores):
+
+        $errorData = ['error' => 'La tabla indicada no existe'];
+        $errorJson = json_encode($errorData);
+        $responseBody = $response -> getBody();
+        $responseBody -> write($errorJson);
+        return $response -> withStatus(404) -> withBody($responseBody);
     });
 
     $app->run();
