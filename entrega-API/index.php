10b68af22c82f6a6187985b61e1658a0f1e2b1d7
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-05-28 10:34:25 -0300

    Se realiza Buscar juegos.

--- entrega-API/index.php
+++ entrega-API/index.php
@@ -463,5 +463,44 @@
 
     //? m) Buscar juegos
 
+    $app -> get('/juegos', function(Request $request, Response $response, $args) use ($db){
+        try{
+            $params = $request -> getQueryParams();
+
+            $query = "SELECT j.nombre as nombrejuego, j.imagen, j.tipo_imagen, j.descripcion, j.url, g.nombre as nombregenero, p.nombre as nombrePlataforma FROM juegos j INNER JOIN generos g ON j.id_genero = g.id INNER JOIN plataformas p ON j.id_plataforma = p.id WHERE 1 = 1";
+
+            if(!empty($params)){
+                $genero = $params["genero"];
+                $plataforma = $params["plataforma"];
+                $nombre = $params["nombre"];
+                $orden = $params["ordenar"];
+                if($genero != ""){
+                    $query = $query." AND g.id = $genero";
+                }
+                if($plataforma != ""){
+                    $query = $query." AND p.id = $plataforma";
+                }
+                if($nombre != ""){
+                    $query = $query." AND j.nombre LIKE '$nombre%'"; // Devuelve todos los matcheos.
+                }
+                if($orden != ""){
+                    $query = $query." ORDER BY j.nombre $orden";
+                }
+            }
+
+            $connection = $db -> getConnection();
+
+            $sqlSelect = $connection -> prepare($query);
+            $sqlSelect -> execute();
+            $data = $sqlSelect -> fetchAll(PDO::FETCH_ASSOC);
+            $jsonData = json_encode($data);
+            $response -> getBody() -> write($jsonData);
+            return $response -> withStatus(200) -> withHeader('Content-Type','Application/json');
+        } catch (Exception $e){
+            //TODO gestionar excepciones.
+        }
+
+    });
+
 
     $app->run();
