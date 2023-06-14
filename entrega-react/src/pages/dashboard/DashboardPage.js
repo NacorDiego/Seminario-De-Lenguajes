f7bfca3205417d1663c118b6f4260492b90eadca
Ema Gomez <emagozz@gmail.com>
2023-06-14 20:34:07 -0300

    Add comments

--- entrega-react/src/pages/dashboard/DashboardPage.js
+++ entrega-react/src/pages/dashboard/DashboardPage.js
@@ -3,7 +3,10 @@ import HeaderComponent from '../../components/HeaderComponent'
 const DashboardPage = () => {
   return (
     <>
+      {/* Aca vamos a controlar todas los headers y nav */}
       <HeaderComponent />
+      {/* Y aca vamos a poner el listado de pages que se van a rendereizar segun el boton que toques en el navbar la idea es usar react-router-dom */}
+      {/* Aca abajo agregar el footer */}
     </>
   )
 }
