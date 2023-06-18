8ab36c28ea4a1cdaf693a6d87b587501bb104c2d
Ema Gomez <emagozz@gmail.com>
2023-06-17 23:08:33 -0300

    Add headers

--- entrega-API/index.php
+++ entrega-API/index.php
@@ -2,6 +2,8 @@
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Slim\Factory\AppFactory;
+    use Slim\Middleware\MethodOverrideMiddleware;
+    use Slim\Routing\RouteCollectorProxy;
 
     //Se encarga de cargar automaticamente todas las clases y dependencias definidas en tu proyecto PHP.
     require __DIR__ . '/vendor/autoload.php';
@@ -18,6 +20,14 @@
     //Investigar como mover todos los endpoints de genero a otro archivo y modularizarlo.
 
     //TODO Reemplazar returns por Excepciones.
+
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    });
 
     //? A) Crear un nuevo género
 
