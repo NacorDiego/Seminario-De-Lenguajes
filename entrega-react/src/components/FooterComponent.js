ce594120dce6b26da62c8c3efccb16c9219be18b
Ema Gomez <emagozz@gmail.com>
2023-06-19 20:42:00 -0300

    Add react-router-dom

--- entrega-react/src/components/FooterComponent.js
+++ entrega-react/src/components/FooterComponent.js
@@ -1,11 +1,11 @@
 import './styles.css'
 
 const FooterComponent = () => {
-    return (
-        <footer class="footer">
-            <span>Diego Ezequiel Nacor - Emanuel Gomez - 2023</span>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <span>Diego Ezequiel Nacor - Emanuel Gomez - 2023</span>
+    </footer>
+  )
 }
 
 export default FooterComponent
