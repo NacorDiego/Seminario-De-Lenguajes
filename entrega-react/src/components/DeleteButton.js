7a3f3d7c7e66614298cb6b4e016bb7c79a874e65
Ema Gomez <emagozz@gmail.com>
2023-06-23 21:17:27 -0300

    Add comments

--- entrega-react/src/components/DeleteButton.js
+++ entrega-react/src/components/DeleteButton.js
@@ -1,20 +1,15 @@
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { BASE_URL } from '../hooks/useFetch'
-import { useState } from 'react'
+
+//Por el momento lo voy a dejar asi nomas pero la idea seria mejorarlo
+// Lo incorpore en juegos pero la idea es usarlo solo en plataformas y generos
 
 const DeleteButton = ({ endpoint, id }) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
-  const [isDeleted, setIsDeleted] = useState(false)
-
   const handleDelete = async event => {
     event.preventDefault()
 
     try {
-      setIsLoading(true)
-      setError(null)
-
       const response = await fetch(`${BASE_URL}${endpoint}/${id}`, { method: 'DELETE' })
 
       console.log('response', response)
@@ -22,12 +17,8 @@ const DeleteButton = ({ endpoint, id }) => {
       if (!response.ok) {
         throw new Error('Failed to delete data')
       }
-
-      setIsDeleted(true)
     } catch (error) {
-      setError(error.message)
-    } finally {
-      setIsLoading(false)
+      console.log('error', error)
     }
   }
 
