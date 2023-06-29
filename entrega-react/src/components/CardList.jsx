3df894f87efb6be181d394b9002727e6db74bd6e
Ema Gomez <emagozz@gmail.com>
2023-06-28 21:34:07 -0300

    Fix minor bug with images

--- entrega-react/src/components/CardList.jsx
+++ entrega-react/src/components/CardList.jsx
@@ -10,7 +10,17 @@ const CardList = ({ results }) => {
               <li className="juego" key={juego.id}>
                 <a href={juego.url} target="_blank" rel="noreferrer">
                   <div className="card">
-                    <img className="imagen-juego" src={juego.imagen} alt="juego" />
+                    <img
+                      className="imagen-juego"
+                      //Nosotros tenemos la URL encodedada en base64 en nuestra DB y aca la decomeamos
+                      //Estoy usando un ternario para ver si la URL ya viene con https o no por si algunos juegos ya viene pre-cargados con imagenes https
+                      src={
+                        juego.imagen.includes('https')
+                          ? juego.imagen
+                          : `data:image/jpeg;base64,${juego.imagen}`
+                      }
+                      alt="juego"
+                    />
                     <div className="card-contenido">
                       <h4 className="titulo-juego">{juego.nombrejuego}</h4>
                       <ul className="lista-encabezados">
